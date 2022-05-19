d0da914145dacbe0c41a1f2d7e7b99c3fafa4ee0
oscarcool <oscarcool@rambler.ru>
2022-05-19 20:10:56 +0300

    added button icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def fa_icon(icon_class)
-    content_tag "span", "", class: "fa fa-#{icon_class}"
+    content_tag "i", "", class: "bi bi-#{icon_class}"
   end
 
   def user_avatar(user)
