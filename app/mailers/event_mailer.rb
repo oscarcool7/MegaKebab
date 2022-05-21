1387b143159ceae189b907e9453670503be04219
oscarcool <oscarcool@rambler.ru>
2022-05-21 13:14:13 +0300

    edited comment and subscription mailer

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,9 +1,9 @@
 class EventMailer < ApplicationMailer
-  def comment(event, comment, email)
+  def comment(comment, email)
     @comment = comment
-    @event = event
+    @event = comment.event
 
-    mail to: email, subject: "Новый комментарий @ #{event.title}"
+    mail to: email, subject: "Новый комментарий у #{@event.title}"
   end
 
   def photo(photo, email)
@@ -13,11 +13,11 @@ class EventMailer < ApplicationMailer
     mail to: email, subject: "Новая фотка у #{@event.title}"
   end
 
-  def subscription(event, subscription)
+  def subscription(subscription)
     @email = subscription.user_email
-    @event = event
+    @event = subscription.event
     @name = subscription.user_name
 
-    mail to: event.user.email, subject: "Новая подписка на #{event.title}"
+    mail to: @event.user.email, subject: "Новая подписка на #{@event.title}"
   end
 end
