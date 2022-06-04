8e8e2e80da92c6b0c69da01e0af3115a03d73bfa
oscarcool <oscarcool@rambler.ru>
2022-06-04 12:29:56 +0300

    added tests to policy

---
+++ spec/policies/event_policy_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe EventPolicy, type: :policy do
+  let(:user_is_an_owner) { User.new }
+  let(:user_is_not_an_owner) { User.new }
+  let(:event) { user_is_an_owner.events.build }
+
+  subject { EventPolicy }
+
+  context "authorized user" do
+    permissions :edit?, :destroy?, :update? do
+      context "is the owner of event" do
+        it "gets the access" do
+          is_expected.to permit(user_is_an_owner, event)
+        end
+      end
+
+      context "is not the owner of event" do
+        it "does not get the access" do
+          is_expected.not_to permit(user_is_not_an_owner, event)
+        end
+      end
+    end
+
+    permissions :show? do
+      it "gets the access" do
+        is_expected.to permit(user_is_an_owner, event)
+        is_expected.to permit(user_is_not_an_owner, event)
+      end
+    end
+  end
+
+  context "unregistered user" do
+    permissions :edit?, :destroy?, :show?, :update? do
+      it "does not get the access" do
+        is_expected.not_to permit(nil, event)
+      end
+    end
+  end
+end
