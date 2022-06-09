75cc40056335a0d34f05084054cc97add7e7c2d0
oscarcool <oscarcool@rambler.ru>
2022-06-09 22:53:17 +0300

    refactoring omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,33 +1,24 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # Дёргаем метод модели, который найдёт пользователя
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
-
-    # Если юзер есть, то логиним и редиректим на его страницу
-    if @user.persisted?
-      flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: "Facebook")
-
-      sign_in_and_redirect @user, event: :authentication
-    # Если неудачно, то выдаём ошибку и редиректим на главную
-    else
-      flash[:error] = I18n.t("devise.omniauth_callbacks.failure", kind: "Facebook", reason: "authentication error")
-
-      redirect_to root_path
-    end
+    provider
   end
 
   def vkontakte
-    # Дёргаем метод модели, который найдёт пользователя
-    @user = User.find_for_vkontakte_oauth(request.env["omniauth.auth"])
+    provider
+  end
 
-    # Если юзер есть, то логиним и редиректим на его страницу
+  private
+
+  def provider
+    @user = User.find_for_oauth(request.env["omniauth.auth"])
+    provider = request.env['omniauth.auth'].provider
+
     if @user.persisted?
-      flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: "Vkontakte")
+      flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: provider)
 
       sign_in_and_redirect @user, event: :authentication
-    # Если неудачно, то выдаём ошибку и редиректим на главную
     else
-      flash[:error] = I18n.t("devise.omniauth_callbacks.failure", kind: "Vkontakte", reason: "authentication error")
+      flash[:error] = I18n.t("devise.omniauth_callbacks.failure", kind: provider, reason: "authentication error")
 
       redirect_to root_path
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,41 +18,31 @@ class User < ApplicationRecord
 
   after_commit :link_subscriptions, on: :create
 
-  def send_devise_notification(notification, *args)
-    devise_mailer.send(notification, self, *args).deliver_later
-  end
-
-  def self.find_for_facebook_oauth(access_token)
-    email = access_token.info.email
-    user = where(email: email).first
-
-    return user if user.present?
-
-    provider = access_token.provider
-    id = access_token.extra.raw_info.id
-    url = "https://facebook.com/#{id}"
-
-    where(url: url, provider: provider).first_or_create! do |user|
-      user.name = access_token.info.name
-      user.email = email
-      user.password = Devise.friendly_token.first(16)
-    end
-  end
-
-  def self.find_for_vkontakte_oauth(access_token)
+  def self.find_for_oauth(access_token)
     email = access_token.info.email.downcase
     user = where(email: email).first
 
     return user if user.present?
 
     provider = access_token.provider
-    url = access_token.info.urls[:Vkontakte]
+    id = access_token.extra.raw_info.id
+
+    case provider
+    when "facebook"
+      url = "https://facebook.com/#{id}"
+    when "vkontakte"
+      url = access_token.info.urls[:Vkontakte]
+    end
 
     where(url: url, provider: provider).first_or_create! do |user|
       user.name = access_token.info.name
       user.email = email
       user.password = Devise.friendly_token.first(16)
     end
+  end
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
   end
 
   private
