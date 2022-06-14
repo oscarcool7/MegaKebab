7286800836a591efc26452c053c162c8339da92f
oscarcool <oscarcool@rambler.ru>
2022-06-14 22:09:26 +0300

    edited resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,6 @@
 Resque.logger = Logger.new(Rails.root.join("log", "#{Rails.env}_resque.log"))
 Resque.logger.level = Logger::DEBUG
+
+uri = URI.parse("redis://localhost:6379")
+REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+Resque.redis = REDIS
